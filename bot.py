c3cb50f97be75e743ea938dafd0480d3806ba599
zeffo <phatakaman@gmail.com>
2022-01-27 15:34:23 +0530

    remove old command

--- bot.py
+++ bot.py
@@ -44,12 +44,6 @@ bot.super = lambda: commands.check(lambda ctx: ctx.author.id in [bot.owner_id, 3
 async def invite(ctx):
     await ctx.send(embed=discord.Embed(description='[Invite me to your server!](https://discord.com/oauth2/authorize?client_id=716818354769362984&scope=bot&permissions=1073212631)\n`Created By Zeffo#9673. Very useless at the moment :D`'))
 
-@bot.command(usage='`hk mnss`')
-async def mnss(ctx):
-    '''A command that sends my class timetable'''
-    embed = discord.Embed()
-    embed.set_image(url='https://cdn.discordapp.com/attachments/756100166825541733/829806414812938250/49780e73-d5fa-402d-bfd5-81499c71061b.png')
-    await ctx.send(embed=embed)
 
 @bot.command()
 async def info(ctx):
